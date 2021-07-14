40545fdf101e2a7f86d6f440adf44565ff6cf792
Joshua Ji <joshua.ji@jiwhiz.com>
2021-07-13 21:10:22 -0600

    Added lerping stuff

--- scrolling-terrain-bg/src/p5/particle.js
+++ scrolling-terrain-bg/src/p5/particle.js
@@ -7,22 +7,28 @@ class Particle {
     constructor() {
         this.x = random(0, width);
         this.y = random(0, height);
-        this.r = 5; // although it changes based on the mouse
+        this.r = 5;
         this.xSpeed = random(-0.05, 0.05);
         this.ySpeed = random(-0.2, -0.3);
     }
 
     // creation of a particle.
     createParticle() {
-        fill('rgb(254, 222, 23)');
+        // gradient from [this website](https://www.schemecolor.com/yellow-orange-gradient.php)
+        const startYellow = color(255, 246, 0)
+        // the end color is also transparent
+        const endOrange = color(254, 129, 22, 0)
+        fill(startYellow);
         circle(this.x, this.y, this.r);
 
         // glow effect using increasingly larger (and less opaque) circles
-        for (let i = 0; i < 10; i++) {
+        // we fill from the outside in to prevent larger shapes from overloading the smaller ones
+        const glowWidth = 15;
+        for (let i = glowWidth; i > 0; i--) {
             const size = this.r + i;
-            const alpha = (15 - size) / 15;
-
-            fill(`rgba(254, 222, 23, ${alpha})`);
+            const alpha = (glowWidth + 5 - size) / (glowWidth + 5);
+            const fillColor = lerpColor(endOrange, startYellow, alpha);
+            fill(fillColor);
             circle(this.x, this.y, size);
         }
     }
@@ -36,6 +42,9 @@ class Particle {
             this.reset()
         this.x += this.xSpeed;
         this.y += this.ySpeed;
+
+        // change the xspeed a bit
+        this.xSpeed += random(-0.01, 0.01);
     }
 
     reset() {
