10583038498b356357dc5cc785898ee046fd8fbb
Joshua Ji <joshua.ji@jiwhiz.com>
2021-07-16 00:30:45 -0600

    Delete p5-ts-starter and og scrolling terrain bg

--- p5-typescript-starter/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-    mode: 'production',
-    // https://webpack.js.org/guides/production/#source-mapping
-    devtool: 'source-map'
-});
