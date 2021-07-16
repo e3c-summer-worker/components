a4c48cf0d4a62976990dbb32a3529bd505df6910
Joshua Ji <joshua.ji@jiwhiz.com>
2021-07-16 15:29:16 -0600

    `npm run start` works - I'm not using webpack dev server anymore pog

--- scrolling-terrain-bg/webpack.dev.js
+++ scrolling-terrain-bg/webpack.dev.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path')
 
 // WARNING: libraries don't work for dev server for some reason
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
+    watch: true,
     devServer: {
         contentBase: path.join(__dirname, 'build'),
     }
