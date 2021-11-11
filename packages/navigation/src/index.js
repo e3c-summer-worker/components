5bb046c975adbd6f44c89655f44930e792209071
Joshua Ji <joshua.ji@jiwhiz.com>
2021-11-10 18:43:14 -0700

    fix(navigation): :bug: Should resolve issue #30

--- packages/navigation/src/index.js
+++ packages/navigation/src/index.js
@@ -2,7 +2,7 @@ import { Elm as DesktopElm } from './DesktopNav.elm';
 import { Elm as MobileElm } from './MobileNav.elm';
 
 
-window.onload = () => {
+window.addEventListener('load', () => {
     // desktop navigation
     const headerNav = document.getElementById('headerNav');
     DesktopElm.DesktopNav.init({ node: headerNav });
@@ -10,4 +10,4 @@ window.onload = () => {
     // mobile navigation
     const sidecarNav = document.getElementById('sidecarNav');
     MobileElm.MobileNav.init({ node: sidecarNav });
-}
+})
