b0d64804b252414af32bbc44a9444cbdafbb2cc5
Joshua Ji <joshua.ji@jiwhiz.com>
2021-12-04 01:22:31 -0700

    Patch terrain. Testing out whatsapp messaging

---
+++ .changeset/soft-avocados-dance.md
@@ -0,0 +1,5 @@
+---
+"@e3c-summer-worker/scrolling-terrain-bg": patch
+---
+
+Patch. Add comments to test out the whatsapp messaging
--- packages/scrolling-terrain-bg/src/Misc.ts
+++ packages/scrolling-terrain-bg/src/Misc.ts
@@ -1,3 +1,10 @@
+/**
+ * Generates a random number between min and max
+ * 
+ * @param min The minimum number
+ * @param max The maximum number
+ * @returns A random number between min and max
+ */
 export const getRandom = (min: number, max: number) => {
     return Math.random() * (max - min) + min;
 }
