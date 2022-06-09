fb9c6b281c731152783c5143013d2e542a6e82c6
Joshua Ji <joshua.ji@jiwhiz.com>
2022-06-09 13:24:54 -0600

    Add gym waiver form links

--- packages/navigation-mandarin/src/NavStructure.elm
+++ packages/navigation-mandarin/src/NavStructure.elm
@@ -97,6 +97,9 @@ navbar =
               { name = "爱福会友资料"
               , link = "/member-documents"
               }
+            , { name = "體育館使用者免責聲明書"
+              , link = "https://eccchurch.ca/gym-waiver-form"
+              }
             ]
         }
       )
